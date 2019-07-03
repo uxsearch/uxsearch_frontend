ea21b1af35d2beec665a7d6b410a4c4fb8672292
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-07-03 15:45:21 +0700

    [UPDATE] develop : update route for record page experimenter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Test from './pages/test'
+import RecordPage from './pages/recordPage'
 import NotFound from './pages/error/not_found.js'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,7 +10,8 @@ class App extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Test} />
+          <Route exact path='/' component={Test} />
+          <Route exact path='/testing/record' component={RecordPage} />
           <Route component={NotFound} />
         </Switch>
       </Router>
