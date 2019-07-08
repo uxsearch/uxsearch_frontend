ac024d8b2dc34325303346d562356398e3a0a324
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-07-08 09:12:08 +0700

    [UPDATE] develop : add button for link to record page

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 class Test extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <h1>Test test page</h1>
+        <a href='/testing/record'>
+          <Button className='btn btn-success'>
+            Start Record
+          </Button>
+        </a>
       </div>
     )
   }
