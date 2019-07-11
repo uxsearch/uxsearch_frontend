4cfc7df7f3b1b51424662c7d63be6ca64d506d17
JAAJJT <jetchuta.3113@mail.kmutt.ac.th>
2019-07-12 00:35:28 +0700

    [CREATE] Show video component

--- src/pages/uxer/videoResult.js
+++ src/pages/uxer/videoResult.js
@@ -2,6 +2,11 @@ import React from 'react'
 import NavbarUXer from '../../components/utils/NavbarUXer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import '../../static/css/uxer/videoresult.css'
+import Camera from '../../components/record/camera'
+import Screen from '../../components/record/screen'
+
 
 
 class VideoResult extends React.Component {
@@ -9,15 +14,36 @@ class VideoResult extends React.Component {
     return (
       <div>
         <NavbarUXer />
-        <h1>Leroy Romero</h1> 
-       
-       
-       
-       
+      
+        <Container className='container-block'>
+          <Row className='video-block'>
+            <Col xs={12}>
+              <Row>
+                <Col xs={12} md={6} className='text-center'>
+                  <Camera />
+                </Col>
+                <Col xs={12} md={6} className='text-center'>
+                  <Screen />
+                </Col>
+              </Row>
+              <br />
+              <Row className='justify-content-center'>
+                <Col xs={12} md={3} className='text-center'>
+                  <Button type='submit' className='btn-Usability-Test-Note' >Pause</Button>
+                </Col>      
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+
+        <experimenterProfile />
+
+
+
 
         
-      
       </div>
+      
     )
   }
 }
