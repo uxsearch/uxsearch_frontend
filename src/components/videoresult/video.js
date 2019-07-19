56d8c1ae9525baeef8e35e19f01be98643248e66
JAAJJT <jetchuta.3113@mail.kmutt.ac.th>
2019-07-19 18:17:58 +0700

    [CREATE] video for show in videoResult

--- src/components/videoresult/video.js
+++ src/components/videoresult/video.js
@@ -16,36 +16,30 @@ class PlayVideo extends React.Component {
     render() {
         return (
             <div>
-                <Container >
-                    <Row className='bg-play-video' >
-                        <Col md={6}>
-                            <video controls className='play-video'>
-                                <source src='http://media.w3.org/2010/05/bunny/movie.mp4' type='video/mp4' />
+                <div className='card-video'>
+                    <Container >
+                        <Row md={6}>
+                            <Col md={6}>
+                                <video controls className='play-video'>
+                                    <source src='http://media.w3.org/2010/05/bunny/movie.mp4' type='video/mp4' />
+                                </video>
+                            </Col>
+                            
+                            <Col md={6} >
+                                <video controls className='play-video'>
+                                    <source src='http://media.w3.org/2010/05/bunny/movie.mp4' type='video/mp4' />
 
-                            </video>
-                        </Col>
-                        <Col md={6}>
-                            <video controls className='play-video'>
-                                <source src='http://media.w3.org/2010/05/bunny/movie.mp4' type='video/mp4' />
+                                </video>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
 
-                            </video>
-                        </Col>
-                    </Row>
-                </Container>
+
+
             </div>
         )
     }
 }
 export default PlayVideo
-
-
-
-
-
-
-
-
-
-
-
 
