4aaeb13f8f6d692584484c091bf04df9f7e85bbd
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-07-22 16:12:35 +0700

    [UPDATE] develop : update navbar uxer

--- src/components/utils/navbarUXer.js
+++ src/components/utils/navbarUXer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Row, Col } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+
 
 import logo from '../../static/img/Logo_White.png'
 import '../../static/sass/navbar.scss'
@@ -10,46 +13,41 @@ class NavbarUxer extends React.Component {
 		super(props);
 
 		this.toggle = this.toggle.bind(this);
+		this.toggleNavbar = this.toggleNavbar.bind(this);
+
 		this.state = {
-			isOpen: false
+			isOpen: false,
+			collapsed: true
 		};
 	}
+
 	toggle() {
 		this.setState({
 			isOpen: !this.state.isOpen
 		});
 	}
+
+	toggleNavbar() {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
+
 	render() {
 		return (
 			<section id='uxer-navbar'>
-				<Navbar dark className='nav-bgColoruxer'>
-					{/* <Row>
-						<Col md>
-						<Nav className="ml-auto" navbar>
+				<Navbar dark className='nav-bgColoruxer d-md-none'>
+					<NavbarBrand href='/' className='mr-auto'>
+						<img className='logo-icon' src={logo} />
+					</NavbarBrand>
+					<NavbarToggler onClick={this.toggleNavbar} className='mr-2' />
+					<Collapse isOpen={!this.state.collapsed} navbar>
+						<Nav navbar>
 							<NavItem>
-								<NavLink>Projects</NavLink>
+								<NavLink href='#'>Projects</NavLink>
 							</NavItem>
 							<NavItem>
-								<NavLink>Activity</NavLink>
-							</NavItem>
-							</Nav>
-						</Col>
-					</Row> */}
-					<Row>
-						<Col>
-							<NavbarBrand href="#">
-								<img className='logo-icon' src={logo} />
-							</NavbarBrand>
-						</Col>
-					</Row>
-					<NavbarToggler onClick={this.toggle} className='hamburger-menu' />
-					<Collapse isOpen={this.state.isOpen} navbar className>
-						<Nav className="ml-auto" navbar>
-							<NavItem>
-								<NavLink>Projects</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink>Activity</NavLink>
+								<NavLink href='#'>Activity</NavLink>
 							</NavItem>
 							<UncontrolledDropdown nav inNavbar>
 								<DropdownToggle nav caret>
@@ -59,6 +57,7 @@ class NavbarUxer extends React.Component {
 									<DropdownItem>
 										My Account
                   </DropdownItem>
+									<DropdownItem divider />
 									<DropdownItem>
 										Sign out
                   </DropdownItem>
@@ -66,6 +65,48 @@ class NavbarUxer extends React.Component {
 							</UncontrolledDropdown>
 						</Nav>
 					</Collapse>
+				</Navbar>
+				<Navbar expand='md' dark className='nav-bgColoruxer d-none d-md-block'>
+					<Row>
+						<Col md={6}>
+							<Nav className='ml-auto' navbar>
+								<NavItem>
+									<NavLink href='#' className='link-text'>Projects</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink href='#' className='link-text'>Activity</NavLink>
+								</NavItem>
+							</Nav>
+						</Col>
+						<Col md={6} className='account-box'>
+							<Nav>
+								<UncontrolledDropdown nav inNavbar className='dropdown-position'>
+									<DropdownToggle nav caret className='link-text'>
+										Uxer 1
+                	</DropdownToggle>
+									<DropdownMenu right>
+										<DropdownItem>
+											My Account
+                  	</DropdownItem>
+										<DropdownItem divider />
+										<DropdownItem>
+											Sign out
+                  	</DropdownItem>
+									</DropdownMenu>
+								</UncontrolledDropdown>
+							</Nav>
+						</Col>
+					</Row>
+					<Row className='space-title'>
+						<Col md={1} className='text-center border-right-logo'>
+							<NavbarBrand href='#'>
+								<img className='logo-icon' src={logo} />
+							</NavbarBrand>
+						</Col>
+						<Col md={11}>
+							<h1>Project Name</h1>
+						</Col>
+					</Row>
 				</Navbar>
 			</section>
 		);
