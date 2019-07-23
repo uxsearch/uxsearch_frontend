f3f00d092102cc88cb0bee240ed643749169d401
JAAJJT <jetchuta.3113@mail.kmutt.ac.th>
2019-07-23 12:02:00 +0700

    [UPDATE] Each Experimenter Component

--- src/components/EachExperimentProfile/eachExpProfile.js
+++ src/components/EachExperimentProfile/eachExpProfile.js
@@ -7,132 +7,277 @@ import '../../static/sass/uxer/experPage.scss'
 
 
 class EachExpPro extends React.Component {
-    render() {
-        return (
-            <div>
-                <Container>
-                    <Row>
-                        <Col md={6}>
-                            <Row>
-                                <Col md={3}>
-                                </Col>
-                                <Col md={4} className='profile justify-content-center align-items-center'>
-                                    <div >
-                                        <img
-                                            src={require('../../static/img/user-solid.svg')}
-                                            width='auto'
-                                            height='40px'
-                                            className="d-inline-block align-top"
-                                            alt="UX Search Logo"
-                                        />
-                                        <Media object data-src="../../static/img/user-solid.svg/171x180" alt="roundedCircle image" />
-                                        <image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-                                    </div>
-                                    <h1> Leroy Romero</h1>
-                                </Col>
+	render() {
+		return (
+			<div className='bg-container'>
+				<Container >
+					<Row>
+						<Col md={6} >
+							<Row >
+								<Col md={3}>
+								</Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1} className='name justify-content-center align-items-center'>
+									<p> Leroy Romero</p>
+                  </Col>
+								</Col>
 
-                                <Col md={4} className='profile'>
-                                    <h1> Ronald Long</h1>
-                                </Col>
-                                <Col md={1}>
-                                </Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Ronald Long</p>
+                  </Col>
+								</Col>
+								<Col md={1}>
+								</Col>
 
-                            </Row>
-                        </Col>
+							</Row>
+						</Col>
 
-                        <Col md={6}>
-                            <Row>
-                                <Col md={4} className='profile'>
-                                    <h1> Lucinda Daniels</h1>
-                                </Col>
+						<Col md={6}>
+							<Row>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Lucinda Daniels</p>
+                  </Col>
+								</Col>
 
-                                <Col md={4} className='profile'>
-                                    <h1> Ann Brady</h1>
-                                </Col>
-                                <Col md={1}>
-                                </Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Ann Brady</p>
+                  </Col>
+								</Col>
+								<Col md={1}>
+								</Col>
 
-                            </Row>
-                        </Col>
-                    </Row>
+							</Row>
+						</Col>
+					</Row>
 
-                    <Row>
-                        <Col md={6}>
-                            <Row>
-                                <Col md={3}>
-                                </Col>
-                                <Col md={4} className='profile'>
-                                    <h1> Engenia Alexander</h1>
-                                </Col>
+					<Row>
+						<Col md={6}>
+							<Row>
+								<Col md={3}>
+								</Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Engenia Alexander</p>
+                  </Col>
+								</Col>
 
-                                <Col md={4} className='profile'>
-                                    <h1> Nell Davidson</h1>
-                                </Col>
-                                <Col md={1}>
-                                </Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Nell Davidson</p>
+                  </Col>
+								</Col>
+								<Col md={1}>
+								</Col>
 
-                            </Row>
-                        </Col>
+							</Row>
+						</Col>
 
-                        <Col md={6}>
-                            <Row>
-                                <Col md={4} className='profile'>
-                                    <h1> Viola Wise</h1>
-                                </Col>
+						<Col md={6}>
+							<Row>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Viola Wise</p>
+                  </Col>
+								</Col>
 
-                                <Col md={4} className='profile'>
-                                    <h1> Jacob Bowman</h1>
-                                </Col>
-                                <Col md={1}>
-                                </Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Jacob Bowman</p>
+                  </Col>
+								</Col>
+								<Col md={1}>
+								</Col>
 
-                            </Row>
-                        </Col>
-                    </Row>
+							</Row>
+						</Col>
+					</Row>
 
-                    <Row>
-                        <Col md={6}>
-                            <Row>
-                                <Col md={3}>
-                                </Col>
-                                <Col md={4} className='profile'>
-                                    <h1> Allie Moran</h1>
-                                </Col>
+					<Row>
+						<Col md={6}>
+							<Row>
+								<Col md={3}>
+								</Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Allie Moran</p>
+                  </Col>
+								</Col>
 
-                                <Col md={4} className='profile'>
-                                    <h1> Scott Norton</h1>
-                                </Col>
-                                <Col md={1}>
-                                </Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Scott Norton</p>
+                  </Col>
+								</Col>
+								<Col md={1}>
+								</Col>
 
-                            </Row>
-                        </Col>
+							</Row>
+						</Col>
 
-                        <Col md={6}>
-                            <Row>
-                                <Col md={4} className='profile'>
-                                    <h1> Roxie Floyd</h1>
-                                </Col>
+						<Col md={6}>
+							<Row>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Roxie Floyd</p>
+                  </Col>
+								</Col>
 
-                                <Col md={4} className='profile'>
-                                    <h1> Willie Henry</h1>
-                                </Col>
-                                <Col md={1}>
-                                </Col>
+								<Col md={4} className='profile justify-content-center align-items-center'>
+									<div >
+										<img
+											src={require('../../static/img/user-solid.svg')}
+											width='auto'
+											height='40px'
+											className="d-inline-block align-top"
+											alt="UX Search Logo"
+										/>
+										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
+										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
+									</div>
+                  <Col md={1}>
+									<p> Willie Henry</p>
+                  </Col>
+								</Col>
+								<Col md={1}>
+								</Col>
 
-                            </Row>
-                        </Col>
-                    </Row>
+							</Row>
+						</Col>
+					</Row>
 
 
 
 
 
 
-                </Container>
-            </div >
-        )
-    }
+				</Container>
+			</div >
+		)
+	}
 }
 
 export default EachExpPro;
