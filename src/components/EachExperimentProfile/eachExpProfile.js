9cf7199f49b9c7e46ef6d4e16653086f1f808d89
JAAJJT <jetchuta.3113@mail.kmutt.ac.th>
2019-07-23 23:44:03 +0700

    [UPDATE] Each Experimenter component

--- src/components/EachExperimentProfile/eachExpProfile.js
+++ src/components/EachExperimentProfile/eachExpProfile.js
@@ -1,283 +1,296 @@
 import React from 'react'
 import { Container, Row, Col, image, Media } from 'reactstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faCopy } from '@fortawesome/free-solid-svg-icons'
+
 
 import '../../static/sass/uxer/experPage.scss'
 
 
 class EachExpPro extends React.Component {
-	render() {
-		return (
-			<div className='bg-container'>
-				<Container >
-					<Row>
-						<Col md={6} >
-							<Row >
-								<Col md={3}>
-								</Col>
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
-                  <Col md={1} className='name justify-content-center align-items-center'>
-									<p> Leroy Romero</p>
-                  </Col>
-								</Col>
+  render() {
+    return (
+      <div className='bg-container'>
+        <Container >
+          <Row className='justify-content-center align-items-center'>
+            <Col md={6} >
+              <Row >
+                <Col md={3}>
+                </Col>
+                <Col md={4} className='profile '>
+                  <div>
+                    <img
+                      src={require('../../static/img/1.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
 
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
+                  <Col md={1} >
+                    <Row >
+                      <div className='name'>
+                        <p> Leroy </p>
+                        <p> Romero</p>
+                      </div>
+                    </Row>
+                  </Col>
+                </Col>
+
+                <Col md={4} className='profile'>
+                  <div >
+                    <img
+                      src={require('../../static/img/2.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
                   <Col md={1}>
-									<p> Ronald Long</p>
+                    <div className='name'>
+                      <p> Ronald </p>
+                      <p> Long   </p>
+                    </div>
                   </Col>
-								</Col>
-								<Col md={1}>
-								</Col>
+                </Col>
+                <Col md={1}>
+                </Col>
+              </Row>
+            </Col>
 
-							</Row>
-						</Col>
+            <Col md={6}>
+              <Row >
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/3.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
 
-						<Col md={6}>
-							<Row>
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
                   <Col md={1}>
-									<p> Lucinda Daniels</p>
+                    <div className='name'>
+                      <p> Lucinda </p>
+                      <p> Daniels</p>
+                    </div >
                   </Col>
-								</Col>
+                </Col>
 
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/4.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
                   <Col md={1}>
-									<p> Ann Brady</p>
+                    <div className='name'>
+                      <p> Ann </p>
+                      <p> Brady</p>
+                    </div >
                   </Col>
-								</Col>
-								<Col md={1}>
-								</Col>
+                </Col>
+                <Col md={1}>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
 
-							</Row>
-						</Col>
-					</Row>
+          <Row>
+            <Col md={6}>
+              <Row>
+                <Col md={3}>
+                </Col>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/5.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
 
-					<Row>
-						<Col md={6}>
-							<Row>
-								<Col md={3}>
-								</Col>
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
                   <Col md={1}>
-									<p> Engenia Alexander</p>
+                    <div className='name'>
+                      <p> Engenia </p>
+                      <p> Alexander</p>
+                    </div >
                   </Col>
-								</Col>
+                </Col>
 
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/6.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
                   <Col md={1}>
-									<p> Nell Davidson</p>
+                    <div className='name'>
+                      <p> Nell </p>
+                      <p> Davidson</p>
+                    </div >
                   </Col>
-								</Col>
-								<Col md={1}>
-								</Col>
+                </Col>
+                <Col md={1}>
+                </Col>
+              </Row>
+            </Col>
 
-							</Row>
-						</Col>
+            <Col md={6}>
+              <Row>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/7.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
 
-						<Col md={6}>
-							<Row>
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
+                  <Col md={1} >
+                    <div className='name'>
+                      <p> Viola </p>
+                      <p> Wise</p>
+                    </div >
+                  </Col>
+                </Col>
+
+                <Col md={4} className='profile '>
+                  <div>
+                    <img
+                      src={require('../../static/img/8.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
+                  <Col md={1} >
+                    <div className='name'>
+                      <p> Jacob </p>
+                      <p> Bowman</p>
+                    </div >
+                  </Col>
+                </Col>
+                <Col md={1}>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={6}>
+              <Row>
+                <Col md={3}>
+                </Col>
+                <Col md={4} className='profile'>
+                  <div >
+                    <img
+                      src={require('../../static/img/9.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
                   <Col md={1}>
-									<p> Viola Wise</p>
+                    <div className='name'>
+                      <p> Allie </p>
+                      <p> Moran</p>
+                    </div >
                   </Col>
-								</Col>
+                </Col>
 
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/10.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
                   <Col md={1}>
-									<p> Jacob Bowman</p>
+                    <div className='name'>
+                      <p> Scott </p>
+                      <p> Norton</p>
+                    </div >
                   </Col>
-								</Col>
-								<Col md={1}>
-								</Col>
+                </Col>
+                <Col md={1}>
+                </Col>
+              </Row>
+            </Col>
 
-							</Row>
-						</Col>
-					</Row>
+            <Col md={6}>
+              <Row>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/11.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
 
-					<Row>
-						<Col md={6}>
-							<Row>
-								<Col md={3}>
-								</Col>
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
                   <Col md={1}>
-									<p> Allie Moran</p>
+                    <div className='name'>
+                      <p> Roxie </p>
+                      <p> Floyd</p>
+                    </div >
                   </Col>
-								</Col>
+                </Col>
 
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
+                <Col md={4} className='profile '>
+                  <div >
+                    <img
+                      src={require('../../static/img/12.jpg')}
+                      width='70px'
+                      height='70px'
+                      className="img d-inline-block align-top"
+                      alt="Profile"
+                    />
+                  </div>
+
                   <Col md={1}>
-									<p> Scott Norton</p>
+                    <div className='name'>
+                      <p> Willie </p>
+                      <p> Henry</p>
+                    </div >
                   </Col>
-								</Col>
-								<Col md={1}>
-								</Col>
-
-							</Row>
-						</Col>
-
-						<Col md={6}>
-							<Row>
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
-                  <Col md={1}>
-									<p> Roxie Floyd</p>
-                  </Col>
-								</Col>
-
-								<Col md={4} className='profile justify-content-center align-items-center'>
-									<div >
-										<img
-											src={require('../../static/img/user-solid.svg')}
-											width='auto'
-											height='40px'
-											className="d-inline-block align-top"
-											alt="UX Search Logo"
-										/>
-										<Media object data-src="../../static/img/user-solid.svg/171x180" alt=" " />
-										<image src={require('../../static/img/23-007.jpg')} src="/171x180" roundedCircle />
-									</div>
-                  <Col md={1}>
-									<p> Willie Henry</p>
-                  </Col>
-								</Col>
-								<Col md={1}>
-								</Col>
-
-							</Row>
-						</Col>
-					</Row>
-
-
-
-
-
-
-				</Container>
-			</div >
-		)
-	}
+                </Col>
+                <Col md={1}>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div >
+    )
+  }
 }
 
 export default EachExpPro;
