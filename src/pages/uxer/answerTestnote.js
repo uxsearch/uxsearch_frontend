4cc33d4efc5db3eacdc662748f4c013fd5b75eb7
JAAJJT <jetchuta.3113@mail.kmutt.ac.th>
2019-10-22 15:19:00 +0700

    [UPDATE] Testnote

--- src/pages/uxer/answerTestnote.js
+++ src/pages/uxer/answerTestnote.js
@@ -8,10 +8,17 @@ import { Checkbox, Radio, RadioGroup, FormControlLabel, withStyles } from '@mate
 
 import NavbarExp from '../../components/utils/navbarExperimenter'
 import NotSupport from '../../components/utils/notSupport'
+import NavbarUXer from '../../components/utils/navbarUXer'
 
 import '../../static/sass/experimenter/answer.scss'
 import axios from '../../utils/axios'
 import APIURI from '../../utils/apiuri'
+
+
+import ExperProfile from '../../components/uxer/videoresult/profileBlock'
+
+import '../../static/sass/uxer/videoResult.scss'
+
 
 const RadioButton = withStyles({
   root: {
@@ -65,6 +72,9 @@ class AnswerTestnote extends React.Component {
       uxerId: '8t6UN47Z749qacrEvZ8O',
       projectId: 'a89OdndRvNEoasnHXfhu',
       experId: match.params.experId,
+      experiment: undefined,
+      answer: [],
+      answerId: '934DlGsmzBo8JlZWoDRL',
       project: undefined,
       testnote: [],
     }
@@ -73,15 +83,30 @@ class AnswerTestnote extends React.Component {
   async componentDidMount() {
     this.getProject()
     this.getTestnote()
+    try {
+      const response = await axios.get(`${APIURI.EXPERIMENTER}${this.state.experId}/`)
+      if (response.status !== 200) {
+        throw new Error('CANNOT GET EXPERIMENT PROFILE')
+      }
+      this.setState({ experiment: response.data.data })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  getAge(birthdate) {
+    const today = new Date()
+    const age = today.getFullYear() - birthdate.getFullYear()
+    return age + ' years'
   }
 
   submitTestnote = async (values) => {
     console.log(values)
-    console.log('test')
+    console.log('test123')
     // try {
-    //   const response = await axios.put(`${APIURI.UXER}${this.state.uxerId}/${APIURI.ONE_PROJECT}${this.state.projectId}/${APIURI.EXPERIMENTER}${this.state.experId}/answer-question/update/`, values)
+    //   const response = await axios.put(`${APIURI.UXER}${this.state.uxerId}/${APIURI.ONE_PROJECT}${this.state.projectId}/${APIURI.EXPERIMENTER}${this.state.experId}/${APIURI.ANSWER}${this.state.answerId}/update/`, values)
     //   if (response.status !== 200) {
-    //     throw new Error('CANNOT SUBMIT QUESTIONNAIRE')
+    //     throw new Error('CANNOT SUBMIT TESTNOTE')
     //   }
     //   this.props.history.push(`/experimenter/${this.state.experId}/thanks`)
     // } catch (e) {
@@ -115,25 +140,66 @@ class AnswerTestnote extends React.Component {
 
   render() {
     const { testnote, project } = this.state
+    const experiment = this.state.experiment
     return (
       <div>
+        <section id='video-result'>
+          <NavbarUXer />
+          <Container fluid className='space-bottom-video'>
+            <Row className='justify-content-center align-items-center'>
+              <Col xs={12} sm={8} md={6} lg={5}>
+                <Row className='align-items-center justify-content-center'>
+                  <Col xs={3} sm={4} xl={3}>
+                    <div className='profile-block'>
+                      <img src='https://picsum.photos/200/300' alt='Profile Picture' className='profile-img' />
+                    </div>
+                  </Col>
+                  <Col xs={9} sm={8} xl={9}>
+                    <Row>
+                      <Col xs={12}>
+                        <p className='no-margin exper-name'>{experiment && `${experiment.firstname} ${experiment.lastname}`}</p>
+                      </Col>
+                    </Row>
+                    <Row className='d-sm-none space-btn-mobile'>
+                      <Col xs={12}>
+                        <Button href={`/uxer/${this.props.uxerId}/project/${this.props.projId}/experiment/answertestnote`} className='btn-usability-test'>Usability Test Note</Button>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+              <Col md={3} xl={4} className='d-none d-md-block' />
+              <Col xs={12} sm={4} md={3} xl={2} className='d-none d-sm-block'>
+                <Button href={`/uxer/${this.props.uxerId}/project/${this.props.projId}/experiment/answertestnote`} className='btn-usability-test w-100'>Usability Test Note</Button>
+              </Col>
+            </Row>
+          </Container>
+          <Container className='space-block'>
+            {experiment &&
+              <>
+                <ExperProfile
+                  name={`${experiment.firstname} ${experiment.lastname}`}
+                  age={this.getAge(new Date(experiment.birthdate.seconds * 1000))}
+                  gender={`${experiment.gender}`}
+                  tel={`${experiment.tel}`}
+                  email={`${experiment.email}`}
+                  city={`${experiment.province}`}
+                  country={`${experiment.country}`}
+                  educate={`${experiment.educate}`}
+                  job={`${experiment.job}`}
+                  lifestyle={`${experiment.lifestyle}`}
+                />
+              </>
+            }
+            <br />
+          </Container>
+        </section>
+
         <NotSupport className='d-md-none' />
         <section id='answer' className='d-none d-md-block'>
-          <NavbarExp />
           <Container>
             <Row>
               <Col xs={12}>
-                <Row className='space-head-block'>
-                  <Col xs={12}>
-                    <p className='title'>'{project && `${project.name}`}' <span>Testnote</span></p>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col xs={12}>
-                    <p>Your can choose your choice. From “UX Search” (Choose your choice on testnote)</p>
-                  </Col>
-                </Row>
-                <br />
                 <Form
                   onSubmit={this.submitTestnote}
                   mutators={{ ...arrayMutators }}
@@ -143,6 +209,12 @@ class AnswerTestnote extends React.Component {
                       <form onSubmit={handleSubmit}>
                         <Row>
                           <Col xs={12} className='answer-block'>
+                            <Row className='space-head-block'>
+                              <Col xs={12}>
+                                <h2 className='title'>'{project && `${project.name}`}' <span>Testnote</span></h2>
+                              </Col>
+                            </Row>
+                              <br />
                             {testnote.map((question, index) => (
                               <>
                                 <Row key={question.id}>
