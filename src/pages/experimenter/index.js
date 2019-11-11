702e5354ca11fc81d84d4a4371c6b51897ede3d5
JAAJJT <jetchuta.3113@mail.kmutt.ac.th>
2019-11-11 22:52:46 +0700

    [ADD] ModalSubmit after click Start Usability testing

--- src/pages/experimenter/index.js
+++ src/pages/experimenter/index.js
@@ -13,36 +13,6 @@ import NotSupport from '../../components/utils/notSupport'
 
 import '../../static/sass/experimenter/index.scss'
 
-const modalSubmit = () => {
-  swal({
-    title: 'Are you sure?',
-    icon: 'warning',
-    buttons: {
-      cancel: {
-        text: 'Cancel',
-        value: null,
-        visible: true,
-      },
-      confirm: {
-        text: 'Confirm',
-        value: true,
-        visible: true,
-      }
-    },
-    dangerMode: false,
-  }).then((willSubmit) => {
-    if (willSubmit) {
-      swal({
-        title: 'Thank you very much',
-        text: 'If you complete test, press button to complete, please.',
-        icon: 'success',
-        timer: 2500,
-        buttons: false
-      });
-    }
-  });
-}
-
 class IndexExperiment extends React.Component {
   constructor(props) {
     super(props)
@@ -89,7 +59,7 @@ class IndexExperiment extends React.Component {
       const newValue = { ...values }
       const prepareEducate = newValue.educate ? newValue.educate : ''
       const prepareJob = newValue.job ? newValue.job : ''
-      if(this.state.country === '') {
+      if (this.state.country === '') {
         newValue.province = ''
       }
 
@@ -106,8 +76,40 @@ class IndexExperiment extends React.Component {
     }
   }
 
+  modalSubmit = () => {
+    swal({
+      title: 'Are you sure?',
+      icon: 'warning',
+      buttons: {
+        cancel: {
+          text: 'Cancel',
+          value: null,
+          visible: true,
+        },
+        confirm: {
+          text: 'Confirm',
+          value: true,
+          visible: true,
+        }
+      },
+      dangerMode: false,
+    }).then((willSubmit) => {
+      if (willSubmit) {
+        swal({
+          title: 'Thank you very much',
+          text: 'If you complete test, press button to complete, please.',
+          icon: 'success',
+          timer: 2500,
+          buttons: false
+        });
+      }
+    });
+  }
+
   render() {
     const project = this.state.project
+    const swal = require('sweetalert')
+
     return (
       <div>
         <NotSupport className='d-md-none' />
@@ -143,7 +145,7 @@ class IndexExperiment extends React.Component {
                         </Row>
                         <Row className='justify-content-center space-btn'>
                           <Col xs={12} md={4} className='text-center'>
-                            <Button className='btn-start-test' type='submit'>Start Usability Testing</Button>
+                            <Button className='btn-start-test' type='submit' onClick={() => this.modalSubmit()} >Start Usability Testing</Button>
                           </Col>
                         </Row>
                       </form>
